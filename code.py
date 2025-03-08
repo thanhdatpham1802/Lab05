28c9be1e4e2d5d8c7f4177c59b856aa708f442a4
thanhdatpham1802 <feeddepro@gmail.com>
2025-03-08 16:24:10 +0700

    Revert "Update function 01"

--- code.py
+++ code.py
@@ -1,8 +1,9 @@
 def f01():
-    print("Hello")
+    print("This is function f01 - Version 1.0")
 
 def f02():
     print("This is function f02 - Version 1.0")
+
 if __name__ == "__main__":
     f01()
     f02()
